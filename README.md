f40ada876bfacc0664515afebfb0c532e90cecd1
Rayzr522 <rayzr522@gmail.com>
2017-01-30 08:53:33 -0500

    Project 'AdvancedList' generated
    Generated using CNLPluginArchetype and custom CLI project generator tool by Rayzr

---
+++ README.md
@@ -0,0 +1,4 @@
+# AdvancedList
+An advanced, prettier, per-role /list for Bukkit.
+
+*Note: This README is a pre-generated stub*
---
+++ src/main/java/com/rayzr522/advancedlist/AdvancedList.java
@@ -0,0 +1,15 @@
+package com.rayzr522.advancedlist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Rayzr
+ */
+public class AdvancedList extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
